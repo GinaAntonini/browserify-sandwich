ca4f28304dc87832be56b9ab41116e2596725ad7
Gina Antonini <ginamantonini@gmail.com>
2017-10-09 16:01:11 -0500

    made the sandwich order appear

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,35 +1,72 @@
 "use strict";
 
-let theWholeSandwich = [];
-
-const loadBread = require('./bread');
-const loadCheese = require('./cheese');
-const loadCondiments = require('./condiments');
-const loadMeat = require('./meat');
-const loadVeggies = require('./veggies');
+const bread = require('./bread');
+const cheese = require('./cheese');
+const condiments = require('./condiments');
+const meats = require('./meat');
+const veggies = require('./veggies');
 
 const errorFunction = () => {
 	console.log("This is broken.");
 };
 
 const whenBreadLoads = function(){
-	theWholeSandwich = JSON.parse(this.responseText).bread;
+	let allTheBread = JSON.parse(this.responseText).bread;
+	let breadString = "";
+	for (var i = 0; i < allTheBread.length; i++) {
+		let bread = allTheBread[i];
+		breadString += `<input id="${bread.type}" type="checkbox" value="${bread.price}">${bread.type}</input>`;
+	}
+
+	$('#breadHolder').html(breadString);
 };
 
 const whenCheeseLoads = function (){
-	theWholeSandwich += JSON.parse(this.responseText).cheese;
+	let allTheCheese = JSON.parse(this.responseText).cheese;
+	let cheeseString = "";
+	for (var i = 0; i < allTheCheese.length; i++) {
+		let cheese = allTheCheese[i];
+		cheeseString += `<input id="${cheese.type}" type="checkbox" value="${cheese.price}">${cheese.type}</input>`;
+	}
+	$('#cheeseHolder').html(cheeseString);
 };
 
 const whenCondimentsLoads = function(){
-	theWholeSandwich += JSON.parse(this.responseText).condiments;
+	let allTheCondiments = JSON.parse(this.responseText).condiments;
+	let condimentsString = "";
+	for (var i = 0; i < allTheCondiments.length; i++) {
+		let condiments = allTheCondiments[i];
+		condimentsString += `<input id="${condiments.type}" type="checkbox" value="${condiments.price}">${condiments.type}</input>`;
+	}
+	$('#condimentsHolder').html(condimentsString);
 };
 
 const whenMeatLoads = function (){
-	theWholeSandwich += JSON.parse(this.responseText).meat;
+	let allTheMeats = JSON.parse(this.responseText).meat;
+	let meatString = "";
+	for (var i = 0; i < allTheMeats.length; i++) {
+		let meats = allTheMeats[i];
+		meatString += `<input id="${meats.type}" type="checkbox" value="${meats.price}">${meats.type}</input>`;
+	}
+	$('#meatHolder').html(meatString);
 };
 
 const whenVeggiesLoads = function (){
-	theWholeSandwich += JSON.parse(this.responseText).veggies;
+	let allTheVeggies = JSON.parse(this.responseText).veggies;
+	let veggiesString = "";
+	for (var i = 0; i < allTheVeggies.length; i++) {
+		let veggies = allTheVeggies[i];
+		veggiesString += `<input id="${veggies.type}" type="checkbox" value="${veggies.price}">${veggies.type}</input>`;
+	}
+	$('#veggiesHolder').html(veggiesString);
 };
 
+const loadMenu = () => {
+	bread(whenBreadLoads, errorFunction);
+	cheese(whenCheeseLoads, errorFunction);
+	condiments(whenCondimentsLoads, errorFunction);
+	meats(whenMeatLoads, errorFunction);
+	veggies(whenVeggiesLoads, errorFunction);
+};
 
+module.exports = {loadMenu};
---
+++ javascripts/events.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+$('#submitOrder').click(function(){
+	let total = 0; //running total
+	let orderedBread = "";
+	$('#breadHolder > input:checked').each(function(index){
+		//use $(this) to access the checked input box
+		let selectedBread = this;
+		//get the id for output to finalOrder
+		orderedBread += `<div>${selectedBread.id}</div>`;
+		//get the value and add to total
+		total += selectedBread.value;
+	});
+	let orderedCheese = "";
+	$('#cheeseHolder > input:checked').each(function(index){
+		let selectedCheese = this;
+		orderedCheese += `<div>${selectedCheese.id}</div>`;
+		total += selectedCheese.value;
+	});
+	let orderedCondiments = "";
+	$('#condimentsHolder > input:checked').each(function(index){
+		let selectedCondiments = this;
+		orderedCondiments += `<div>${selectedCondiments.id}</div>`;
+		total += selectedCondiments.value;
+	});
+	let orderedMeats = "";
+	$('#meatHolder > input:checked').each(function(index){
+		let selectedMeats = this;
+		orderedMeats += `<div>${selectedMeats.id}</div>`;
+		total += selectedMeats.value;
+	});
+	let orderedVeggies = "";
+	$('#veggiesHolder > input:checked').each(function(index){
+		let selectedVeggies = this;
+		orderedVeggies += `<div>${selectedVeggies.id}</div>`;
+		total += selectedVeggies.value;
+	});
+
+	//display the final order and total on the page
+	let finalOrder = orderedBread + orderedCheese + orderedCondiments + orderedMeats + orderedVeggies; 
+	$('#finalOrder').html(`<h4>Pay This Amount: $ ${total}</h4><p>Final Order:<p></p> ${finalOrder}</p>`);
+
+});
+
+module.exports = {};
